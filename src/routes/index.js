29ab0e3e6197e9e1973a1f333576442de85ccab0
esthernascimento <esthersolzinhacarambola@gmail.com>
2026-04-21 14:57:43 -0300

    Telas estáticas da dashboard com rotas funcionando!

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 router.get("/", function (req, res) {
     res.render("index");
@@ -11,6 +12,19 @@ router.get("/cultura", function (req, res) {
 
 router.get("/sobre", function (req, res) {
     res.render("sobre");
+});
+
+/* rotas da dash */
+router.get("/dashboard-adm", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../../public/dashboard/dashboard-adm.html"));
+});
+
+router.get("/perfil-adm", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../../public/dashboard/perfil-adm.html"));
+});
+
+router.get("/criar-post", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../../public/dashboard/criar-post.html"));
 });
 
 module.exports = router;
